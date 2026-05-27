9e5ed5b93568039de564c484e7e801b7187b7b08
Artem Doronin <a.doronin2@g.nsu.ru>
2026-05-27 09:20:20 +0700

    Change 6 task

--- 6/main.cpp
+++ 6/main.cpp
@@ -6,10 +6,16 @@
 #include <boost/program_options.hpp>
 
 using namespace std;
-namespace po = boost::program_options;
 using namespace std::chrono;
 
-void initialize_boundaries(vector<vector<double>> &grid, int N)
+namespace po = boost::program_options;
+
+inline int idx(int i, int j, int N)
+{
+    return i * N + j;
+}
+
+void initialize_boundaries(vector<double> &grid, int N)
 {
     double top_left = 10.0;
     double top_right = 20.0;
@@ -17,36 +23,56 @@ void initialize_boundaries(vector<vector<double>> &grid, int N)
     double bottom_left = 20.0;
 
     for (int j = 0; j < N; j++)
-        grid[0][j] = top_left + (top_right - top_left) * j / (N - 1);
+    {
+        grid[idx(0, j, N)] =
+            top_left + (top_right - top_left) * j / (N - 1);
+    }
 
     for (int j = 0; j < N; j++)
-        grid[N - 1][j] = bottom_left + (bottom_right - bottom_left) * j / (N - 1);
+    {
+        grid[idx(N - 1, j, N)] =
+            bottom_left + (bottom_right - bottom_left) * j / (N - 1);
+    }
 
     for (int i = 0; i < N; i++)
-        grid[i][0] = top_left + (bottom_left - top_left) * i / (N - 1);
+    {
+        grid[idx(i, 0, N)] =
+            top_left + (bottom_left - top_left) * i / (N - 1);
+    }
 
     for (int i = 0; i < N; i++)
-        grid[i][N - 1] = top_right + (bottom_right - top_right) * i / (N - 1);
+    {
+        grid[idx(i, N - 1, N)] =
+            top_right + (bottom_right - top_right) * i / (N - 1);
+    }
 }
 
-void save_matrix(const vector<vector<double>> &grid, const string &filename)
+void save_matrix(const vector<double> &grid,
+                 int N,
+                 const string &filename)
 {
     ofstream out(filename);
 
-    for (const auto &row : grid)
+    for (int i = 0; i < N; i++)
     {
-        for (double val : row)
-            out << val << " ";
+        for (int j = 0; j < N; j++)
+        {
+            out << grid[idx(i, j, N)] << " ";
+        }
+
         out << "\n";
     }
 }
 
-void print_matrix(const vector<vector<double>> &grid)
+void print_matrix(const vector<double> &grid, int N)
 {
-    for (const auto &row : grid)
+    for (int i = 0; i < N; i++)
     {
-        for (double val : row)
-            printf("%8.2f ", val);
+        for (int j = 0; j < N; j++)
+        {
+            printf("%8.2f ", grid[idx(i, j, N)]);
+        }
+
         cout << endl;
     }
 }
@@ -61,9 +87,12 @@ int main(int argc, char *argv[])
     {
         po::options_description desc("Options");
 
-        desc.add_options()("help,h", "show help")("size", po::value<int>(&N)->default_value(128),
-                                                  "grid size")("iter", po::value<int>(&max_iter)->default_value(1000000),
-                                                               "max iterations")("eps", po::value<double>(&eps)->default_value(1e-6),
+        desc.add_options()("help,h", "show help")("size",
+                                                  po::value<int>(&N)->default_value(128),
+                                                  "grid size")("iter",
+                                                               po::value<int>(&max_iter)->default_value(1000000),
+                                                               "max iterations")("eps",
+                                                                                 po::value<double>(&eps)->default_value(1e-6),
                                                                                  "accuracy");
 
         po::variables_map vm;
@@ -86,8 +115,8 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    vector<vector<double>> grid(N, vector<double>(N, 0.0));
-    vector<vector<double>> new_grid(N, vector<double>(N, 0.0));
+    vector<double> grid(N * N, 0.0);
+    vector<double> new_grid(N * N, 0.0);
 
     initialize_boundaries(grid, N);
     initialize_boundaries(new_grid, N);
@@ -108,24 +137,23 @@ int main(int argc, char *argv[])
             {
                 for (int j = 1; j < N - 1; j++)
                 {
-                    new_grid[i][j] = 0.25 * (grid[i - 1][j] +
-                                             grid[i + 1][j] +
-                                             grid[i][j - 1] +
-                                             grid[i][j + 1]);
+                    new_grid[idx(i, j, N)] =
+                        0.25 * (grid[idx(i - 1, j, N)] +
+                                grid[idx(i + 1, j, N)] +
+                                grid[idx(i, j - 1, N)] +
+                                grid[idx(i, j + 1, N)]);
 
-                    double diff = fabs(new_grid[i][j] - grid[i][j]);
+                    double diff =
+                        fabs(
+                            new_grid[idx(i, j, N)] -
+                            grid[idx(i, j, N)]);
 
                     if (diff > error)
                         error = diff;
                 }
             }
 
-#pragma acc parallel loop collapse(2)
-            for (int i = 1; i < N - 1; i++)
-            {
-                for (int j = 1; j < N - 1; j++)
-                    grid[i][j] = new_grid[i][j];
-            }
+            swap(grid, new_grid);
 
             if (error < eps)
                 break;
@@ -141,10 +169,12 @@ int main(int argc, char *argv[])
     cout << "Error: " << error << endl;
     cout << "Time: " << elapsed << " ms" << endl;
 
-    save_matrix(grid, "result.txt");
+    save_matrix(grid, N, "result.txt");
 
     if (N == 10 || N == 13)
-        print_matrix(grid);
+    {
+        print_matrix(grid, N);
+    }
 
     return 0;
 }
