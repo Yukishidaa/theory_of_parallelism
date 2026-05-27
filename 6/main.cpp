a6894268dff09bdb88db9c83fd8d251f3ff5b46c
Artem Doronin <a.doronin2@g.nsu.ru>
2026-05-27 09:50:48 +0700

    Change 6 task

--- 6/main.cpp
+++ 6/main.cpp
@@ -71,7 +71,7 @@ int main(int argc, char *argv[])
         po::store(po::parse_command_line(argc, argv, desc), vm);
         po::notify(vm);
 
-        if (vm.count("help"))
+        if (vm.count("helpp"))
         {
             cout << desc << endl;
             return 0;
